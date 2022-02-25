cdbe7159dcbc0ef0e82bd690ec3620669240bfeb
remember5n <remember5n@gmail.com>
2022-02-26 01:34:58 +0200

    added _redirect file

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchData = async () => {
     try {
 
-        const response = await axios.get('http://api.mediastack.com/v1/news?access_key=6c140bfd35d2c2526206f4e7f5ee1f74')
+        const response = await axios.get(`/api/news?access_key=6c140bfd35d2c2526206f4e7f5ee1f74`)
         return response.data.data;
 
     } catch (error) {
