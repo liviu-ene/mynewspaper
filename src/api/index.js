9cd4715aed0656836d4ca9b14eeb91487fb30f61
remember5n <remember5n@gmail.com>
2022-02-26 01:44:05 +0200

    test newsapi

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,10 @@ import axios from "axios";
 export const fetchData = async () => {
     try {
 
-        const response = await axios.get(`/api/news?access_key=6c140bfd35d2c2526206f4e7f5ee1f74`)
-        return response.data.data;
+        //const response = await axios.get(`/api/news?access_key=6c140bfd35d2c2526206f4e7f5ee1f74`)
+        const response = await axios.get(`/api/everything?q=tesla&from=2022-01-25&sortBy=publishedAt&apiKey=82e03435cd8645e2b69c56da31aedab8`)
+        console.log(response);
+        return response.data.articles;
 
     } catch (error) {
         console.log(error)
